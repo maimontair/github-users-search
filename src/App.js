091174c3f2bb678ff1c95360b15f26cbb2d5cc86
Claude Maimon <claudemaimon@Claudes-Air.lan>
2022-12-21 13:24:40 -1000

    add basic router and update to react router 6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Dashboard />}></Route>
+          <Route path='login' element={<Login />}></Route>
+          <Route path='*' element={<Error />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
